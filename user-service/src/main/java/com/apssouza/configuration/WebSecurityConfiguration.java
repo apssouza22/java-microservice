20e3d71eb582b8dd90a692d9abb5fe0534776360
Alexsandro Souza <apssouza22@gmail.com>
2017-04-07 17:05:32 +0100

    created auth service separated using JWT

--- user-service/src/main/java/com/apssouza/configuration/WebSecurityConfiguration.java
+++ user-service/src/main/java/com/apssouza/configuration/WebSecurityConfiguration.java
@@ -1,42 +1,71 @@
 package com.apssouza.configuration;
 
 import com.apssouza.repositories.AccountRepository;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author apssouza
  */
 @Configuration
-class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AccountRepository accountRepository;
 
     @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
+    @Bean
+    public AuthenticationManager    authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        return (email) -> accountRepository
-                .findByEmail(email)
-                .map(a -> new User(
-                        a.getEmail(),
-                        a.getPassword(),
-                        true, true, true, true,
-                        AuthorityUtils.createAuthorityList("USER", "write")
-                ))
-                .orElseThrow(
-                        () -> new UsernameNotFoundException("could not find the user '" + email + "'")
-                );
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) -> {
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                        }
+                )
+            .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+            .and()
+                .httpBasic();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("reader")
+                .password("reader")
+                .authorities("FOO_READ")
+                .and()
+                .withUser("writer")
+                .password("writer")
+                .authorities("FOO_READ", "FOO_WRITE");
+    }
+
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        return (email) -> accountRepository
+//                .findByEmail(email)
+//                .map(a -> new User(
+//                        a.getEmail(),
+//                        a.getPassword(),
+//                        true, true, true, true,
+//                        AuthorityUtils.createAuthorityList("USER", "write")
+//                ))
+//                .orElseThrow(
+//                        () -> new UsernameNotFoundException("could not find the user '" + email + "'")
+//                );
+//    }
 }
