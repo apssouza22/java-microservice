2702b580c4e2ee9446dc6e1613c3c77f1d9b0665
Alexsandro Souza <apssouza22@gmail.com>
2017-04-09 15:03:59 +0100

    added jwt configuration to remainer service

---
+++ remainder-service/src/main/java/com/apssouza/configuration/JwtConfiguration.java
@@ -0,0 +1,44 @@
+package com.apssouza.configuration;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.FileCopyUtils;
+
+/**
+ *
+ * @author apssouza
+ */
+@Configuration
+public class JwtConfiguration {
+
+    @Autowired
+    JwtAccessTokenConverter jwtAccessTokenConverter;
+
+    @Bean
+    @Qualifier("tokenStore")
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    protected JwtAccessTokenConverter jwtTokenEnhancer() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        Resource resource = new ClassPathResource("public.cert");
+        String publicKey = null;
+        try {
+            publicKey = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
+        return converter;
+    }
+}
---
+++ remainder-service/src/main/java/com/apssouza/configuration/OAuth2ResourceServerConfiguration.java
@@ -0,0 +1,44 @@
+
+package com.apssouza.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+/**
+ *
+ * @author apssouza
+ */
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .antMatchers(HttpMethod.GET, "/todos").hasAuthority("USER_READ");
+                //.antMatchers(HttpMethod.POST, "/foo").hasAuthority("FOO_WRITE");
+                //you can implement it like this, but I show method invocation security on write
+    }
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("todo").tokenStore(tokenStore);
+    }
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @Autowired
+    JwtAccessTokenConverter tokenConverter;
+}
