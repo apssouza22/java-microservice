bc62ac4d61a80906cef5e7557a2ee8d51faf33fc
Alexsandro Souza <apssouza22@gmail.com>
2017-10-14 20:07:46 +0100

    added fabric8 plugin to build the REMINDER SERVICE using maven

--- package-projects.sh
+++ package-projects.sh
@@ -3,7 +3,9 @@
 set -ex
 
 mvn clean package
+mvn -f ./remainder-service/pom.xml -Pdockerimage docker:build 
 mvn -f ./eureka-server/pom.xml clean package 
+mvn -f ./config-server/pom.xml clean package 
 mvn -f ./oauth-server/pom.xml clean package 
 mvn -f ./user-service/pom.xml clean package 
 mvn -f ./api-gateway/pom.xml clean package 
