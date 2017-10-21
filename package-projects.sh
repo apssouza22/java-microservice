642f700db892f6b03b87f0935baf93a2c7f14507
Alexsandro Souza <apssouza22@gmail.com>
2017-10-21 18:50:53 +0100

    removed turbine from the project, we will monitor in the admin server

--- package-projects.sh
+++ package-projects.sh
@@ -9,6 +9,5 @@ mvn -f ./config-server/pom.xml  -Pdockerimage docker:build
 mvn -f ./oauth-server/pom.xml  -Pdockerimage docker:build 
 mvn -f ./user-service/pom.xml  -Pdockerimage docker:build 
 mvn -f ./api-gateway/pom.xml  -Pdockerimage docker:build 
-mvn -f ./turbine-monitoring/pom.xml clean package 
 mvn -f ./admin-server/pom.xml  -Pdockerimage docker:build 
 mvn -f ./mail-service/pom.xml  -Pdockerimage docker:build
