9d1567de790bd57ffa8bbf6df754f8f45c459dff
Alexsandro Souza <apssouza22@gmail.com>
2017-11-18 14:10:41 +0000

    added Angular webapp

---
+++ webapp/app/app.routing.ts
@@ -0,0 +1,15 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+import { AuthGuard } from './_guards/index';
+
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
