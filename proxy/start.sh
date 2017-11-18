b1511b3ad9f9a8252d1dbf2c7953ef2f801d8a0e
Alexsandro Souza <apssouza22@gmail.com>
2017-11-18 15:06:16 +0000

    removed proxy service, moved into a new branch

--- proxy/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
-/etc/init.d/filebeat start
-nginx
-tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
